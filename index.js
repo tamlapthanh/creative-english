1dd38aaea564b3703d3cbe1fb51548e35be4c36c
nvsu-laptop\Administrator <nguyen.van.su@quantic.com.vn>
2024-11-12 14:26:40 +0700

    ignore service worker

--- index.js
+++ index.js
@@ -141,12 +141,12 @@ async function getDataFromGoogleSheet() {
     loadDataToDiv(JSON_DATA, ".data_class", "bai_hoc");
 }
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js').then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        }).catch((error) => {
-            console.log('Service Worker registration failed:', error);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('service-worker.js').then((registration) => {
+//             console.log('Service Worker registered with scope:', registration.scope);
+//         }).catch((error) => {
+//             console.log('Service Worker registration failed:', error);
+//         });
+//     });
+// }
